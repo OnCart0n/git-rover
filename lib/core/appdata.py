7cd43342d19b165a62a9ff34eda7b0c63d5abf98
OnCart0n <32082618+OnCart0n@users.noreply.github.com>
2021-01-08 16:00:15 +0800

    实例化统一放到appdata中

--- lib/core/appdata.py
+++ lib/core/appdata.py
@@ -2,10 +2,12 @@
 # -*- encoding: utf-8 -*-
 
 
-from lib.core.config import config
+from lib.core.config import Config
 from lib.module._fofa import Fofa
 from lib.module._shodan import ShodanPro
 
+
+config = Config()
 
 fofa = Fofa(config.fofa.email, config.fofa.key)
 shodan = ShodanPro(config.shodan.key)
