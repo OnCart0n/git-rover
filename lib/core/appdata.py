e86ff93e00c6e26b8cd6e475696d941fd23c1c67
OnCart0n <32082618+OnCart0n@users.noreply.github.com>
2021-01-08 12:37:08 +0800

    全局统一调用数据

---
+++ lib/core/appdata.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+'''
+@Author  :   OnCart0n
+'''
+
+
+from lib.core.config import config
+from lib.module._fofa import Fofa
+from lib.module._shodan import ShodanPro
+
+
+fofa = Fofa(config.fofa.email, config.fofa.key)
+shodan = ShodanPro(config.shodan.key)
