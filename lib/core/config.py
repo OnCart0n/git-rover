05b3e6b1a94c06cec5ea5f1d03889ac6853ef2ca
OnCart0n <32082618+OnCart0n@users.noreply.github.com>
2021-01-08 11:21:39 +0800

    完善config处理

--- lib/core/config.py
+++ lib/core/config.py
@@ -2,11 +2,19 @@
 # -*- encoding: utf-8 -*-
 
 
+config_file = 'config.ini'
+from configparser import ParsingError
+
 class BaseConfig:
+    # 邪教写法
     import configparser as _
     config = _.ConfigParser()
-    config.read('config.ini')
-    
+    try:
+        config.read(config_file)
+    except ParsingError as e:
+        print("请检查配置文件: ", repr(e).split('\n')[1].replace("\t", '').replace(' \\n', ''))
+        exit()
+
     def __init__(self):
         for key in list(set(dir(self))-set(dir(BaseConfig))):
             t = getattr(self, key)
@@ -16,7 +24,6 @@ class BaseConfig:
                 setattr(self, key, self.config.get(self.__class__.__name__, key))
             elif t is bool:
                 setattr(self, key, self.config.getboolean(self.__class__.__name__, key))
-            # print(self.__class__.__name__, getattr(self, key))
 
 
 class App(BaseConfig):
